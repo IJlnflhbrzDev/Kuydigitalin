e73897f1b523da2fe89e72247dcfce49c1a3a66f
IJlnflhbrzDev <IJlnflhbrzDev@gmail.com>
2020-12-31 18:18:18 +0700

    update  navbar and header & responsive typography & image =>  navbar & header

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// Membuat pekondisian sebuah  icont  humberger mobile
 const navbar_toggler_icon = document.querySelector('.icofont-navigation-menu');
 navbar_toggler_icon.addEventListener('click',function () {
      if (navbar_toggler_icon.classList.contains('icofont-navigation-menu')) {
@@ -6,4 +7,17 @@ navbar_toggler_icon.addEventListener('click',function () {
           navbar_toggler_icon.classList.add('icofont-navigation-menu')
      }
 
-})
+});
+
+
+// =========================================== animated schroll changed navbar active toggle ===========================================
+
+const navbar = document.querySelector('.navbar'); ///mengambil sebuah class pembungkus navbar
+window.onscroll = function () {
+     let topWindowSchorllingChanged = window.scrollY; //membuat sbuah variabel dimana web sedang di schroll
+     if (topWindowSchorllingChanged > 100) { //
+          navbar.classList.add('active_changed_navbar')
+     }else {
+           navbar.classList.remove('active_changed_navbar')
+     }
+}
