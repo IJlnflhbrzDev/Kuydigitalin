2214fda070a350e4b33360a91282ea2cea646c31
IJlnflhbrzDev <IJlnflhbrzDev@gmail.com>
2021-01-06 13:20:12 +0700

    remove portfolio and remove animation filter image

--- src/script.js
+++ src/script.js
@@ -23,27 +23,6 @@ window.onscroll = function () {
      }
 }
 
-
-// / =========================================== animated image filter portfolio      ===========================================
-var gridItems = document.querySelectorAll('.item')
-function showAll() {
-    gridItems.forEach(function(element) {
-        element.classList.add('show')
-    })
-}
-
-function showCategory(category) {
-    gridItems.forEach(function(element){
-        if (element.classList.contains(category)) {
-            element.classList.add('show')
-        }
-        else {
-            element.classList.remove('show')
-        }
-    })
-}
-showAll()
-
 // / =========================================== animated preloader      ===========================================
 var preloader = document.getElementById('loader');
 window.addEventListener('load' ,function () {
